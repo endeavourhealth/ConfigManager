f2f3bf6cbf0f63afd336ac71b430d91d0122cb5e
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-05-15 18:10:29 +0100

    Fix build warnings

--- src/configmanager/src/main/java/org/endeavourhealth/common/config/ConfigManagerException.java
+++ src/configmanager/src/main/java/org/endeavourhealth/common/config/ConfigManagerException.java
@@ -1,6 +1,8 @@
 package org.endeavourhealth.common.config;
 
 public class ConfigManagerException extends Exception {
+    static final long serialVersionUID = 1L;
+
 	public ConfigManagerException(String message, Exception exception) {
 		super(message, exception);
 	}
