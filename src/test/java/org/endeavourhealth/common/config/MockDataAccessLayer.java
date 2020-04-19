91859504b29228ba6f5863be4083c5f8681294d1
Drew Littler <drewlittler@hotmail.com>
2020-04-19 10:28:15 +0100

    Adding new fn to enumerate records

--- src/test/java/org/endeavourhealth/common/config/MockDataAccessLayer.java
+++ src/test/java/org/endeavourhealth/common/config/MockDataAccessLayer.java
@@ -3,6 +3,7 @@ package org.endeavourhealth.common.config;
 import org.endeavourhealth.common.config.dataAccess.DataAccessLayer;
 
 import java.util.Map;
+import java.util.Set;
 
 public class MockDataAccessLayer implements DataAccessLayer {
 	private String data;
@@ -24,6 +25,11 @@ public class MockDataAccessLayer implements DataAccessLayer {
 
 	@Override
 	public Map<String, String> getConfigurations(String appIdParam) {
+		return null;
+	}
+
+	@Override
+	public Set<String> getAppIds() {
 		return null;
 	}
 }
