7a285b4b7d4d4b1f97dd146f4f289fbac7a73368
Antar Upadhyaya <antar.upadhyaya@gmail.com>
2020-11-29 20:53:10 +0000

    BNF Chapter Update SD 158

---
+++ database/config-database/mySQL/manual-scripts/utility-postcode-updater.sql
@@ -0,0 +1,14 @@
+INSERT INTO config.config
+(app_id, config_id, config_data)
+VALUES
+('global', 'bnf_snomed_web_site_url', 'https://www.nhsbsa.nhs.uk/prescription-data/understanding-our-data/bnf-snomed-mapping');
+
+INSERT INTO config.config
+(app_id, config_id, config_data)
+VALUES
+('global', 'bnf_snomed_zip_file_base_url', 'https://www.nhsbsa.nhs.uk');
+
+INSERT INTO config.config
+(app_id, config_id, config_data)
+VALUES ('global', 'bnf_snomed_local_path', 'D:\\\\NHS\\\\Data\\\\test\\\\');
+
